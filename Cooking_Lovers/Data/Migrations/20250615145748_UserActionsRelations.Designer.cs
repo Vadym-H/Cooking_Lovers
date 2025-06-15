5298fe7ae09564e5943852b95316e8c1dee6551f
Pasha Sergiychuk <pashasergiychuk30@gmail.com>
2025-06-15 17:08:17 +0200

    feat: add like & save recipes endpoints

--- Cooking_Lovers/Data/Migrations/20250615145748_UserActionsRelations.Designer.cs
+++ Cooking_Lovers/Data/Migrations/20250615145748_UserActionsRelations.Designer.cs
@@ -4,6 +4,7 @@ using Cooking_Lovers.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cooking_Lovers.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250615145748_UserActionsRelations")]
+    partial class UserActionsRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
