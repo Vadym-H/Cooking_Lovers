98dbf5146859972639c97267a9334ed3a89c2614
Pasha Sergiychuk <pashasergiychuk30@gmail.com>
2025-06-15 16:46:22 +0200

    feat: user actions table

---
+++ Cooking_Lovers/Data/Migrations/20250615142834_UserActions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cooking_Lovers.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserActions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserActions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HasLiked = table.Column<bool>(type: "bit", nullable: false),
+                    HasSaved = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserActions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserActions_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserActions_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActions_RecipeId",
+                table: "UserActions",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActions_UserId_RecipeId",
+                table: "UserActions",
+                columns: new[] { "UserId", "RecipeId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserActions");
+        }
+    }
+}
